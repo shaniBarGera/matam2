a4100c3f334ed80236927f4fc7f5d3a12f6206f0
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2018-05-02 23:50:05 +0300

    Add files via upload

---
+++ season.h
@@ -0,0 +1,33 @@
+/*
+ * season.h
+ *
+ *  Created on: Apr 14, 2018
+ *      Author: muhamadg
+ */
+
+#ifndef SEASON_H_
+#define SEASON_H_
+
+typedef struct season* Season;
+
+#include"team.h"
+#include"driver.h"
+
+
+typedef enum seasonStatus {
+    SEASON_OK,
+    SEASON_MEMORY_ERROR,
+    BAD_SEASON_INFO,
+    SEASON_NULL_PTR} SeasonStatus;
+
+Season SeasonCreate(SeasonStatus* status, const char* season_info);
+void   SeasonDestroy(Season season);
+Driver SeasonGetDriverByPosition(Season season, int position, SeasonStatus* status);
+Driver* SeasonGetDriversStandings(Season season);
+Team SeasonGetTeamByPosition(Season season, int position, SeasonStatus* status);
+Team* SeasonGetTeamsStandings(Season season);
+int SeasonGetNumberOfDrivers(Season season);
+int SeasonGetNumberOfTeams(Season season);
+SeasonStatus SeasonAddRaceResult(Season season, int* results);
+
+#endif /* SEASON_H_ */
